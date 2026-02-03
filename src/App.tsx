134285fd3c09e13279cfa6c382bc51fd1edc12c3
Julianna <juliannaroberts18@gmail.com>
2026-02-03 17:51:26 -0600

    add game styling and UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,70 @@
 import { useState } from "react";
-import { createGame, makeMove } from "./tic-tac-toe";
+import { createGame, makeMove, getWinner } from "./tic-tac-toe";
 
 function App() {
   let [gameState, setGameState] = useState(getInitialGame())
 
-  // TODO: display the gameState, and call `makeMove` when a player clicks a button
-  return <div>Hello World! current player: {gameState.currentPlayer}</div>;
+ const winningmessageColor = getWinner(gameState) === "X" ? "#D4785A" : getWinner(gameState) === "O" ? "#0C4B4A" :"#7A6B3A"
+ const messageColor = gameState.currentPlayer === "X" ? "#D4785A" : gameState.currentPlayer === "O" ? "#0C4B4A" :"#7A6B3A"
+
+  return <div style={{
+              display: "flex", 
+              flexDirection:"column", 
+              alignItems: "center", 
+              justifyContent: "center", 
+              height: "100vh", 
+              fontFamily: "Courier New", 
+              color: "#7A6B3A", 
+              fontSize: "30px"}}>
+    {!getWinner(gameState) && !gameState.board.every((cell) => cell !== null) && <p style={{color:messageColor}}>Current player: {gameState.currentPlayer} </p>}
+    {getWinner(gameState) && <p style={{color:winningmessageColor}}>{getWinner(gameState)} wins! Woo!</p>}
+    {!getWinner(gameState) && gameState.board.every((cell) => cell !== null) && <p style={{color:winningmessageColor}}>Wahh it's a tie...</p> }
+    <table style={{border: "1px solid #DBBCB4", borderCollapse: "collapse" }}>
+      <tbody>
+       {[0, 3, 6].map((rowStart) => (
+        <tr key={rowStart}>
+          {[0, 1, 2].map((col) => {
+            const position = rowStart + col 
+            return (
+              <td 
+                key={position}
+                style={{
+                  border:"1px solid #DBBCB4",
+                  width: "150px", height: "150px", 
+                  textAlign: "center", 
+                  fontFamily: "Courier New", 
+                  color: gameState.board[position] === "X" ? "#D4785A":"#0C4B4A",
+                  fontSize: "80px",
+                 }}
+                onClick={() => {
+                  if (gameState.board[position] === null) {
+                    setGameState(makeMove(gameState, position))
+                  }
+                }}
+              >
+                {gameState.board[position]}
+                </td>
+            )
+          })}
+        </tr>
+       ))}
+      </tbody>
+    </table>
+      {(getWinner(gameState) || gameState.board.every((cell) => cell !== null)) && 
+      <button 
+      style={{
+        backgroundColor: "#DBBCB4",
+        padding: "20px, 40px",
+        margin: "40px"
+        }}
+      
+      onClick={() => setGameState(createGame())}>Play Again!</button>
+      }
+    </div>;
 }
 
 function getInitialGame() {
   let initialGameState = createGame()
-  initialGameState = makeMove(initialGameState, 3)
-  initialGameState = makeMove(initialGameState, 0)
   return initialGameState
 }
 
