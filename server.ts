90547f70a47ce39f5344d807890180ea7ece4831
paris <paris@macbookpro.mynetworksettings.com>
2026-02-05 11:32:29 -0500

    use port from env var if present

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import ViteExpress from 'vite-express';
 import { app } from './src/server-app.js';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 ViteExpress.listen(app, PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
